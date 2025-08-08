226e1ecf4438d0a6d3a02de8caa3eacdb441354c
GameXost <dsergey010206@gmail.com>
2025-08-09 00:56:22 +0300

    recursion added

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
-	"grokAlgs/sorts/selection"
+	rec "grokAlgs/recursion"
 )
 
 func main() {
-	array := []int{1, 3, 2, 5, 4, 2, 2, 4, 5}
+	// array := []int{1, 3, 2, 5, 4, 2, 2, 4, 5}
+	// // fmt.Println(array)
+	// sortedArray := selection.SelectionSort(array)
+	// fmt.Println(sortedArray)
+	// // fmt.Println(binary.BinarySearch(sortedArray, 2))
+	// selection.SelectionSortInPLace(array)
 	// fmt.Println(array)
-	sortedArray := selection.SelectionSort(array)
-	fmt.Println(sortedArray)
-	// fmt.Println(binary.BinarySearch(sortedArray, 2))
-	selection.SelectionSortInPLace(array)
-	fmt.Println(array)
+
+	// rec.CountdownReversed(10)
+	// fmt.Println(rec.Sum([]int{1, 2, 3, 4}))
+	fmt.Println(rec.Greatest([]int{1, 2, 3, 4, 5, 6, 7, 8}))
 }
